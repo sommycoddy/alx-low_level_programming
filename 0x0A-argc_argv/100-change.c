25f4b239b039656cd69d4da20b8cad8830b547b4
sommycoddy <madukatochi94@gmail.com>
2023-03-13 09:13:32 -0700

    100-change.c{

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,12 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
- * main - prints the number of arguments passed into it.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - function that prints a minimum number of coins
+ * @argc: number of args supplied to program
+ * @argv: array of pointers to the args
+ * Return: -1 or 0
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
-	return (0);
+        int cents, coins = 0;
+
+        if (argc != 2)
+        {
+                printf("Error\n");
+                return (1);
+        }
+
+        cents = atoi(argv[1]);
+
+        while (cents > 0)
+        {
+                coins++;
+                if ((cents - 25) >= 0)
+                {
+ cents -= 25;
+                        continue;
+                }
+                if ((cents - 10) >= 0)
+                {
+                        cents -= 10;
+                        continue;
+                }
+                if ((cents - 5) >= 0)
+                {
+                        cents -= 5;
+                        continue;
+                }
+                if ((cents - 2) >= 0)
+                {
+                        cents -= 2;
+                        continue;
+                }
+                cents--;
+        }
+
+        printf("%d\n", coins);
+
+        return (0);
 }
+

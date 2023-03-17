ae0081c7f0172e6ee7e78c3cc0d03c2f581ce588
sommycoddy <madukatochi94@gmail.com>
2023-03-16 19:48:45 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,10 +5,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _strlen(char *str);
-unsigned long int mul(unsigned long int a, unsigned long int b);
-int main(int argc, char *argv[]);
 
 #endif
